390cc7ac43c58ad22b53bf93c2f35b8becbeed90
ChubbyIrishChick <82422712+ChubbyIrishChick@users.noreply.github.com>
2024-03-30 16:41:12 -0400

    final and third commit

--- js/script.js
+++ js/script.js
@@ -8,9 +8,29 @@ const img = document.querySelector(".img");
 const getImage = async function () {
      const res = await fetch("https://picsum.photos/v2/list?limit=100");
      const images = await res.json();
-          console.log(images);
+     selectRandomImage(images);     
+    
 };
 
-getImage();
+const selectRandomImage = function (images) {
+     const randomIndex = Math.floor(Math.random() * images.length);
+      //console.log(randomIndex);
+     const randomImage = images[randomIndex];
+    //console.log(randomImage);
+    displayImage(randomImage);
 
+};
+
+const displayImage = function (randomImage) {
+     const author = randomImage.author;
+     const imageAddress = randomImage.download_url;
+     authorSpan.innerText = author;
+     img.src = imageAddress;
+     imgDiv.classList.remove("hide");
+
+};
+
+button.addEventListener("click", function () {
+     getImage();
+});
 
